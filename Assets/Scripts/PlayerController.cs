90348ae4bc0641882b753464e3ff86bcda0f08a3
Andrea <andrea.antillon.b@gmail.com>
2020-03-03 13:59:45 -0700

    player movement and bullets implemented.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        // if (verticalMovement != 0f || horizontalMovement != 0f) {
+        //     Vector2 newPosition = new Vector2(0f, (verticalMovement * 0.1f));
+        //     transform.Translate(newPosition);
+        // }
+
+        // if (horizontalMovement != 0f) {
+        //     Vector2 newPosition = new Vector2((horizontalMovement * 0.1f), 0f);
+        //     transform.Translate(newPosition);
+        // }
+
+        if (verticalMovement != 0f || horizontalMovement != 0f) {
+            Vector2 newPosition = new Vector2(horizontalMovement * 0.1f, verticalMovement * 0.1f);
+            transform.Translate(newPosition);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Pew");
+
+            Instantiate(projectile, transform.position + new Vector3(0f, 2f, 0f), transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float projectileSpeed = 0.5f;
+    public int maxYPosition = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(0f, projectileSpeed));
+
+        if (transform.position.y > maxYPosition) {
+            Destroy(gameObject);
+        }
+    }
+}
